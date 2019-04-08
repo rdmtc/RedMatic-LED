80bd53f4029c2e9abc0729f6ef3e8758c0fa9d9a
hobbyquaker <bastiraff@googlemail.com>
2019-04-08 19:02:49 +0200

    adapt to raspberrymatic monit

--- nodes/ccu-led.js
+++ nodes/ccu-led.js
@@ -6,6 +6,8 @@ module.exports = function (RED) {
         constructor(config) {
             RED.nodes.createNode(this, config);
 
+            this.monit = fs.existsSync('/usr/bin/monit');
+
             this.on('input', msg => {
                 switch (String(msg.payload).toLowerCase()) {
                     case 'red':
@@ -33,7 +35,7 @@ module.exports = function (RED) {
                         this.manual(0, 0, 0);
                         break;
                     case 'auto':
-                        this.auto();
+                        this.startHssLed();
                         break;
                     default:
                 }
@@ -41,21 +43,66 @@ module.exports = function (RED) {
         }
 
         manual(r, g, b) {
-            exec('pkill hss_led', () => {
+            this.stopHssLed().then(() => {
                 fs.writeFile('/sys/class/leds/rpi_rf_mod:red/brightness', String(r), () => {});
                 fs.writeFile('/sys/class/leds/rpi_rf_mod:green/brightness', String(g), () => {});
                 fs.writeFile('/sys/class/leds/rpi_rf_mod:blue/brightness', String(b), () => {});
             });
         }
 
-        auto() {
-            exec('pkill hss_led', () => {
-                spawn('/bin/hss_led', {
-                    stdio: 'ignore',
-                    detached: true
-                });
+        stopHssLed() {
+            return new Promise(resolve => {
+                if (this.hssLedStopped) {
+                    resolve();
+                } else {
+                    this.unmonitor().then(() => {
+                        exec('/sbin/start-stop-daemon -K -q -p /var/run/hss_led.pid', () => {
+                            this.hssLedStopped = true;
+                            resolve();
+                        });
+                    });
+                }
             });
         }
+
+        startHssLed() {
+            return new Promise(resolve => {
+                if (this.hssLedStopped) {
+                    exec('/sbin/start-stop-daemon -S -q -b -m -p /var/run/hss_led.pid --exec /bin/hss_led -- -l 6', () => {
+                        this.hssLedStopped = false;
+                        this.monitor().then(resolve);
+                    });
+                } else {
+                    resolve();
+                }
+            });
+        }
+
+        unmonitor() {
+            return new Promise(resolve => {
+                if (this.monit) {
+                    exec('monit unmonitor hss_led', () => {
+                        resolve();
+                    });
+                } else {
+                    resolve();
+                }
+            });
+        }
+
+        monitor() {
+            return new Promise(resolve => {
+                if (this.monit) {
+                    exec('monit monitor hss_led', () => {
+                        resolve();
+                    });
+                } else {
+                    resolve();
+                }
+            });
+        }
+
+
     }
 
     RED.nodes.registerType('ccu-led', CcuLedNode);
